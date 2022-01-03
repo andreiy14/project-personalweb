af61d71595f85b453e93ff0653a4ebc15b06d396
andreiy14 <andreiyroring@gmail.com>
2022-01-03 23:03:59 +0700

    last

--- connection/db.js
+++ connection/db.js
@@ -3,5 +3,5 @@ const dbPool = new Pool({
    connectionString : 'postgres://wydgdiwfoygpsi:9d4a3b4c1329c48165d0aeed93cbddd3a0c28500a5f815039eb21b7dae402511@ec2-18-235-235-165.compute-1.amazonaws.com:5432/deu9diao78kehu',
    ssl: {rejectUnauthorized:false}
    
-,})
+})
 module.exports = dbPool
