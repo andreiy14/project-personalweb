215596c38703a85d7938db58f86d2976d6ccb91b
andreiy14 <andreiyroring@gmail.com>
2022-01-04 15:08:05 +0700

    make ittt bettert

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bcrypt =  require('bcrypt')
 const session = require('express-session')
 const flash = require('express-flash')
 const app = express()
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000
 const upload = require('./middleware/uploadFile')
 const { port } = require('pg/lib/defaults')
 
@@ -236,8 +236,8 @@ app.post('/blog',upload.single('image'), function(req,res){
     })
 })
 
-app.listen(PORT,function(){
-    console.log(`running on port : ${PORT}`);
+app.listen(port,function(){
+    console.log(`running on port : ${port}`);
 })
 function getFulltime (time){
     let date = time.getDate()
